eac0496a42214995a266a655df02313c197452de
Kinimotto29 <dommin5@interia.pl>
2017-11-26 09:26:49 +0100

    progres4

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,26 @@
 function obliczZysk(wplata, iloscLat, oprocentowanie, podatek, okresKapitalizacji) {
+//  alternatywa dla if
+    var oprocentowanieOpodatkowane = (podatek)?(oprocentowanie*0.81):oprocentowanie;
+
+//  odpowiadający powyzszemu zapisowi if-else
+//    if(podatek) {
+//         var oprocentowanieOpodatkowane = oprocentowanie * 0.81;
+//    } else {
+//        var oprocentowanieOpodatkowane = oprocentowanie;
+//    }
     
-    if (podatek.checked = true) {
-        wynik = parseFloat(wynik)*19)/100);
-  } else {
-      
-  }
+    
+    var kapital = wplata * Math.pow(1 + (oprocentowanieOpodatkowane / okresKapitalizacji), iloscLat * okresKapitalizacji);
+    
+//toFixed - zaokrąglanie liczb po przecinku
+    return kapital.toFixed(2);
     
 }
 
 function oblicz() {
     
+    
+//  pobieranie zmiennych z formularza i parsowanie do floata
     var wplata = parseFloat(document.getElementById('wplata').value);
     
     var iloscLat = parseFloat(document.getElementById('ilosc-lat').value);
@@ -19,9 +30,9 @@ function oblicz() {
     var oprocentowanie = parseFloat(document.getElementById('oprocentowanie').value) / 100;
     
     var podatek = document.getElementById('podatek').checked;
-       
-    var okresKapitalizacji = parseFloat(document.getElementById('okres-kapitalizacji').value;)
-    
+
     var wynik = obliczZysk(wplata, iloscLat, oprocentowanie, podatek, okresKapitalizacji);
     
+    document.getElementById('wynik').innerHTML = 'Twoja stopa zwrotu to ' + wynik + 'pln';
 }
+
